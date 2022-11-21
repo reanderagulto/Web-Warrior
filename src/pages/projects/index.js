d8a928e76b0df0c669c9acc31cc115c6506c2e2c
Reander Agulto <reanderagulto29@gmail.com>
2022-11-21 22:56:09 +0800

    Markdown files to HTML

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,14 +1,44 @@
+import { graphql, Link } from 'gatsby';
 import React from 'react'
 import Layout from '../../components/Layout'
-import { portfolio } from '../../styles/projects.module.css';
+import { portfolio, project } from '../../styles/projects.module.css';
 
-export default function Projects() {
+export default function Projects({ data }) {
+
+  console.log(data);
+  const projects = data.allMarkdownRemark.nodes
+
   return (
     <Layout>
       <div className={portfolio}>
           <h2>Portfolio</h2>
           <h3>Projects & website I've created</h3>
+          <div className={project}>
+            {projects.map(project => (
+              <Link to={"/projects/" + project.frontmatter.slug} key={project.id}>
+                <div>
+                  <h3>{project.frontmatter.title}</h3>
+                  <p>{project.frontmatter.stack}</p>
+                </div>
+              </Link>
+            ))}
+          </div>
       </div>
     </Layout>
   )
 }
+
+// Export Page Query
+export const query = graphql`
+query Projects {
+  allMarkdownRemark {
+    nodes {
+      frontmatter {
+        title
+        stack
+        slug
+      }
+      id
+    }
+  }
+}`;
